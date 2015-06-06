dfbde5d8fc8fcd852b0def31bae5704f021fdb1f
ok2ju <soulSpringg@gmail.com>
2015-06-06 17:03:02 +0300

    repo init

---
+++ demo.js
@@ -0,0 +1,12 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+$(document).ready(function() {
+  $('#test').particleground({
+    dotColor: '#9BD8F1',
+    lineColor: '#9ce3ff',
+    parallax: false
+  });
+});
